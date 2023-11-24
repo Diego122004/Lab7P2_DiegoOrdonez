b267b028c34840629ccc23a361c2d31667062681
Diego122004 <daof.04@mail.com>
2023-11-24 15:54:35 -0600

    clases

---
+++ src/lab7p2_diegoordonez/Restaurante.java
@@ -0,0 +1,12 @@
+
+package lab7p2_diegoordonez;
+
+import java.util.ArrayList;
+
+
+public class Restaurante {
+    
+    private String nombre;
+    private String ubi;
+    private ArrayList <Producto> productos = new ArrayList();
+}
